86e45d629d8dd9b3a33b64006d4b91adafeaf310
ktrebor <robert.kedves.90@gmail.com>
2022-01-04 10:31:26 +0200

    added generateSquares functions and clearAll

--- script.js
+++ script.js
@@ -1,2 +1,35 @@
+const container = document.querySelector(".container");
+const clear = document.querySelector("#clear");
+
+//on load execute the following functions:
+window.onload = () => {
+    generateSquares()
+}
+
+//Event Listeners
+clear.addEventListener('click', clearAll);
+
+//generate squares inside the container
+function generateSquares (size = 16) {
+    container.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    container.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+    
+    let totalSize = size * size;
+    for (let i = 0; i < totalSize; i++) {
+        let smallSquare = document.createElement('div');
+        smallSquare.classList.add("squares");
+        smallSquare.addEventListener('mouseover', changeColor);
+        container.appendChild(smallSquare);
+    }
+}
+
+//clear all collors 
+function clearAll () {
+    container.textContent = "";
+    generateSquares();
+}
 
 
+function changeColor (e) {
+    e.target.style.backgroundColor = "red";
+}
